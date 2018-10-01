f75dc767fa558a88ca2ba2c3634477988bc48316
Marie Findlay <mariealicefindlay@gmail.com>
2018-10-01 19:01:28 +0100

    version-2

--- src/components/CocktailList.vue
+++ src/components/CocktailList.vue
@@ -1,14 +1,18 @@
 <template>
-    <div class="outerDiv">
-        <ul
-        v-for="(cocktail, index) in cocktails"
-        style="cursor:pointer"
-        @click="updateSelectedCocktail(cocktail)"
-        :key="index"
-        >
-            <li :class="{active: isActive(cocktail)}">{{ cocktail.name.toUpperCase() }}</li>
-        </ul>
+  <div class="outerDiv">
+    <input v-model="search" placeholder="SEARCH" type="text">
+    <ul
+      v-for="(cocktail, index) in paginatedCocktails"
+      style="cursor:pointer"
+      @click="updateSelectedCocktail(cocktail)"
+      :key="index">
+        <li :class="{active: isActive(cocktail)}">{{ cocktail.name.toUpperCase() }}</li>
+    </ul>
+    <div class="previousNext">
+      <button class="prev" :disabled="currentPage==0" @click="previousPage">PREVIOUS</button>
+      <button class = "next" :disabled="currentPage >= numberOfPages" @click="nextPage">NEXT</button>
     </div>
+  </div>
 </template>
 
 <script>
@@ -16,6 +20,11 @@ export default {
   props: ["selectedCocktail"],
   data: function() {
     return {
+      search: null,
+      currentPage: 0,
+      size: 8,
+      nextButtonActive: false,
+      previousButtonActive: true,
       cocktails: [
         {
           name: "Long Island Iced Tea",
@@ -43,15 +52,19 @@ export default {
             {
               name: "Coca cola",
               amount: 1,
-              unit: "dashes"
+              unit: "dash"
             },
             {
               name: "Lemon peel",
               amount: 1,
-              unit: "twists"
+              unit: "twist"
             }
           ],
-          instructions: ["Mix ingredients together"]
+          instructions: [
+            "Mix all contents in a highball glass and sitr gently",
+            "Add dash of Coca-Cola for the coloring",
+            "Garnish with lemon or lime twist"
+          ]
         },
         {
           name: "Daiquiri",
@@ -64,10 +77,10 @@ export default {
             {
               name: "Lime juice",
               amount: 0.5,
-              unit: "limes"
+              unit: "lime"
             },
             {
-              name: "Powdered sugar",
+              name: "granulated sugar",
               amount: 1,
               unit: "tsp"
             }
@@ -94,8 +107,13 @@ export default {
             {
               name: "Sugar syrup",
               amount: 1,
-              unit: "dashes"
+              unit: "dash"
             }
+          ],
+          instructions: [
+            "Pour ingredients into shaker filled with ice",
+            "Shake vigorously",
+            "Strain into chilled martini glass"
           ]
         },
         {
@@ -109,27 +127,27 @@ export default {
             {
               name: "Angostura bitters",
               amount: 2,
-              unit: "dashes"
+              unit: "dash"
             },
             {
               name: "Sugar cube",
               amount: 1,
-              unit: "cubes"
+              unit: "cube"
             },
             {
               name: "Water",
               amount: 1,
-              unit: "dashes"
+              unit: "dash"
             },
             {
               name: "Orange peel",
               amount: 1,
-              unit: "twists"
+              unit: "twist"
             },
             {
               name: "Cocktail cherry",
               amount: 1,
-              unit: "cherries"
+              unit: "cherry"
             }
           ],
           instructions: [
@@ -172,17 +190,17 @@ export default {
             {
               name: "Lemon juice",
               amount: 0.5,
-              unit: "lemons"
+              unit: "lemon"
             },
             {
-              name: "Powdered sugar",
+              name: "granulated sugar",
               amount: 0.5,
               unit: "tsp"
             },
             {
               name: "Cocktail cherry",
               amount: 1,
-              unit: "cherries"
+              unit: "cherry"
             },
             {
               name: "Lemon slice",
@@ -212,12 +230,12 @@ export default {
             {
               name: "Lime juice",
               amount: 0.3,
-              unit: "limes"
+              unit: "lime"
             },
             {
               name: "Salt",
               amount: 1,
-              unit: "dashes"
+              unit: "dash"
             }
           ],
           instructions: [
@@ -243,22 +261,22 @@ export default {
             {
               name: "Angostura bitters",
               amount: 1,
-              unit: "dashes"
+              unit: "dash"
             },
             {
               name: "Ice",
               amount: 3,
-              unit: "cubes"
+              unit: "cube"
             },
             {
               name: "Cocktail cherry",
               amount: 1,
-              unit: "cherries"
+              unit: "cherry"
             },
             {
               name: "Orange peel",
               amount: 1,
-              unit: "twists"
+              unit: "twist"
             }
           ],
           instructions: [
@@ -304,27 +322,292 @@ export default {
             {
               name: "Lime juice",
               amount: 1,
-              unit: "limes"
+              unit: "lime"
             },
             {
-              name: "Powdered sugar",
+              name: "granulated sugar",
               amount: 2,
               unit: "tsp"
             },
             {
               name: "Mint",
               amount: 3,
-              unit: "leaves"
+              unit: "leaf"
             },
             {
               name: "Soda water",
               amount: 1,
-              unit: "dashes"
+              unit: "dash"
             }
+          ],
+          instructions: [
+            "Muddle mint leaves with sugar and lime juice",
+            "Add a splash of soda water and fill the glass with cracked ice",
+            "Pour the rum and top with soda water",
+            "Garnish and serve with straw"
+          ]
+        },
+        {
+          name: "Boulevardier",
+          ingredients: [
+            {
+              name: "Campari",
+              amount: 1,
+              unit: "oz"
+            },
+            {
+              name: "Sweet vermouth",
+              amount: 1,
+              unit: "oz"
+            },
+            {
+              name: "Rye whiskey",
+              amount: 1.25,
+              unit: "oz"
+            },
+            {
+              name: "Orange peel",
+              amount: 1,
+              unit: "twist"
+            }
+          ],
+          instructions: ["Stir with ice, strain, garnish and serve"]
+        },
+        {
+          name: "Sazerac",
+          ingredients: [
+            {
+              name: "Ricard",
+              amount: 1,
+              unit: "tsp"
+            },
+            {
+              name: "granulated sugar",
+              amount: 1,
+              unit: "tsp"
+            },
+            {
+              name: "Peychaud bitters",
+              amount: 2,
+              unit: "dash"
+            },
+            {
+              name: "Water",
+              amount: 2,
+              unit: "oz"
+            },
+            {
+              name: "Bourbon",
+              amount: 2,
+              unit: "oz"
+            },
+            {
+              name: "Lemon peel",
+              amount: 1,
+              unit: "twist"
+            }
+          ],
+          instructions: [
+            "Rinse a chilled old-fashioned glass with the absinthe, add crushed ice, and set it aside",
+            "Stir the remaining ingredients over ice and set it aside",
+            "Discard the ice and any excess absinthe from the prepared glass, and strain the drink into the glass",
+            "Add the lemon peel for garnish"
+          ]
+        },
+        {
+          name: "Bloody Mary",
+          ingredients: [
+            {
+              name: "Vodka",
+              amount: 1.5,
+              unit: "oz"
+            },
+            {
+              name: "Tomato juice",
+              amount: 3,
+              unit: "oz"
+            },
+            {
+              name: "Lemon juice",
+              amount: 1,
+              unit: "dash"
+            },
+            {
+              name: "Worcestershire sauce",
+              amount: 0.5,
+              unit: "tsp"
+            },
+            {
+              name: "Tabasco sauce",
+              amount: 3,
+              unit: "drop"
+            },
+            {
+              name: "Lime slice",
+              amount: 1,
+              unit: "slice"
+            }
+          ],
+          instructions: [
+            "Stirring gently, pour all ingredients into highball glass",
+            "Garnish"
+          ]
+        },
+        {
+          name: "Penecillin",
+          ingredients: [
+            {
+              name: "Blended scotch",
+              amount: 2,
+              unit: "oz"
+            },
+            {
+              name: "Lemon juice",
+              amount: 0.75,
+              unit: "oz"
+            },
+            {
+              name: "Honey syrup",
+              amount: 2,
+              unit: "tsp"
+            },
+            {
+              name: "Ginger syrup",
+              amount: 2,
+              unit: "tsp"
+            },
+            {
+              name: "Islay single malt scotch",
+              amount: 0.25,
+              unit: "oz"
+            }
+          ],
+          instructions: [
+            "Shake blended Scotch, lemon juice, honey syrup and ginger syrup with ice",
+            "Strain over large ice in chilled rocks glass",
+            "Float smoky Scotch on top (be sure to use a smoky Scotch such as an Islay single malt)",
+            "Garnish with candied ginger"
+          ]
+        },
+        {
+          name: "Gin Fizz",
+          ingredients: [
+            {
+              name: "Gin",
+              amount: 2,
+              unit: "oz"
+            },
+            {
+              name: "Lemon juice",
+              amount: 0.5,
+              unit: "lemon"
+            },
+            {
+              name: "granulated sugar",
+              amount: 1,
+              unit: "tsp"
+            },
+            {
+              name: "Soda water",
+              amount: 1,
+              unit: "dash"
+            }
+          ],
+          instructions: [
+            "Shake all ingredients with ice cubes, except soda water",
+            "Pour into glass",
+            "Top with soda water"
+          ]
+        },
+        {
+          name: "Mai Tai",
+          ingredients: [
+            {
+              name: "Light rum",
+              amount: 1,
+              unit: "oz"
+            },
+            {
+              name: "Orgeat syrup",
+              amount: 0.5,
+              unit: "oz"
+            },
+            {
+              name: "Triple sec",
+              amount: 0.5,
+              unit: "oz"
+            },
+            {
+              name: "sweet and sour",
+              amount: 1.5,
+              unit: "oz"
+            },
+            {
+              name: "cherry",
+              amount: 1,
+              unit: "cherry"
+            }
+          ],
+          instructions: [
+            "Shake all ingredients with ice",
+            "Strain into glass",
+            "Garnish and serve with straw"
+          ]
+        },
+        {
+          name: "Pisco Sour",
+          ingredients: [
+            {
+              name: "Pisco",
+              amount: 2,
+              unit: "oz"
+            },
+            {
+              name: "Lemon juice",
+              amount: 1,
+              unit: "oz"
+            },
+            {
+              name: "Granulated sugar",
+              amount: 6,
+              unit: "tsp"
+            }
+          ],
+          instructions: [
+            "Vigorously shake and strain contents in a cocktail shaker with ice cubes",
+            "Pour into glass and garnish with bitters"
           ]
         }
       ]
     };
+  },
+  computed: {
+    sortedCocktails() {
+      return this.cocktails.sort((a, b) => {
+        if (a.name < b.name) return -1;
+        if (a.name > b.name) return 1;
+        return 0;
+      });
+    },
+    filteredCocktails() {
+      if (this.search == null) {
+        return this.sortedCocktails;
+      } else {
+        return this.sortedCocktails.filter(cocktail => {
+          return cocktail.name
+            .toUpperCase()
+            .startsWith(this.search.toUpperCase());
+        });
+      }
+    },
+    numberOfPages() {
+      return Math.floor(this.filteredCocktails.length / this.size);
+    },
+    paginatedCocktails() {
+      let start = this.currentPage * this.size;
+      let end = start + this.size;
+      return this.filteredCocktails.slice(start, end);
+    }
   },
   methods: {
     updateSelectedCocktail(cocktail) {
@@ -334,6 +617,12 @@ export default {
       if (this.selectedCocktail == cocktail) {
         return true;
       } else return false;
+    },
+    nextPage() {
+      this.currentPage++;
+    },
+    previousPage() {
+      this.currentPage--;
     }
   }
 };
@@ -341,25 +630,86 @@ export default {
 
 <style lang="scss" scoped>
 @import "../masterglass2/src/styles/app.scss";
-
 .outerDiv {
   color: $lilac;
+  display: flex;
+  flex-direction: column;
 }
-.active {
-  @include button(250px, 40px, $yellow, 20px);
-  padding-bottom: 5px;
-  padding-left: 10px;
+:disabled {
+  background-color: $lightYellow;
+  color: grey;
+}
+input {
+  @include button(245px, 40px, $lightYellow, 17px);
+  text-align: left;
+  padding-left: 14px;
+  cursor: text;
   margin-left: 0px;
+  margin-bottom: 10px;
+  margin-top: 10px;
+  color: $darkNavy;
+  text-transform: uppercase;
+  outline-style: none;
+  border-radius: 1px 7px 1px 1px;
+  @include mobile {
+    margin-left: 30px;
+    margin-top: -10px;
+    height: 30px;
+    font-size: 15px;
+  }
 }
 ul {
   list-style: none;
   padding: 0;
   margin: 0;
+  @include mobile {
+    margin-left: 30px;
+  }
 }
 li {
   @include h3;
-  line-height: 50px;
+  line-height: 49px;
   vertical-align: top;
+  padding-left: 10px;
+  @include mobile {
+    line-height: 30px;
+    font-size: 15px;
+  }
+}
+.active {
+  @include button(250px, 35px, $yellow, 19px);
+  padding-bottom: 10px;
+  padding-top: 0px;
+  padding-left: 10px;
+  margin-left: 0px;
+  border-radius: 1px 1px 1px 1px;
+  @include mobile {
+    padding-bottom: 0px;
+    font-size: 17px;
+    height: 30px;
+  }
+}
+button {
+  @include button(260px, 40px, $yellow, 17px);
+  margin-top: 10px;
+  width: 130px;
+  margin-bottom: 0px;
+  @include mobile {
+    height: 30px;
+    font-size: 15px;
+    margin-left: 30px;
+    width: 130px;
+  }
+}
+.previousNext {
+  margin-left: 0px;
+}
+.prev {
+  border-radius: 1px 0px 0px 7px;
+}
+.next {
+  margin-left: -4px;
+  border-radius: 0px 1px 1px 0px;
 }
 </style>
 
--- src/components/SelectedCocktailView.vue
+++ src/components/SelectedCocktailView.vue
@@ -10,13 +10,18 @@
             <div class="ingredients">
                 <h3>INGREDIENTS</h3>
                 <table v-for="(ingredient, index) in selectedCocktail.ingredients" :key="index">
-                <tr><td class="col-1">{{ ingredient.name.toUpperCase() }}</td><td class="col-2">{{ ingredient.amount }} {{ingredient.unit}}</td></tr>
+                    <tr>
+                        <td class="col-1">{{ ingredient.name.toUpperCase() }}</td>
+                        <td class="col-2">{{ convertOzToMl(ingredient.amount, ingredient.unit) }} {{ pluraliseUnit(ingredient.amount, ingredient.unit)}}</td>
+                    </tr>
                 </table>
             </div>
         </div>
         <div class="userInput">
             <input v-model="servings" placeholder="NUMBER OF SERVINGS" type="number">
-            <button @click="addToShoppingList(shoppingList, selectedCocktail.name, selectedCocktail.ingredients)">ADD TO SHOPPING LIST</button>
+            <button @click="addToShoppingList(shoppingList, selectedCocktail.name, selectedCocktail.ingredients)">
+                ADD TO SHOPPING LIST
+            </button>
         </div>
     </div>
 </template>
@@ -30,10 +35,17 @@
   height: 500px;
   border-right: $darkNavy solid 1px;
   border-bottom: $darkNavy solid 1px;
+  @include mobile {
+    display: inline;
+    border: none;
+  }
 }
 .content {
   display: flex;
   height: 500px;
+  @include mobile {
+    display: inline;
+  }
 }
 .instructions {
   flex: 1;
@@ -41,40 +53,16 @@
     padding: 0px;
     margin: 0px 50px 15px 0px;
     line-height: 25px;
+    @include mobile {
+      font-size: 15px;
+      margin-bottom: 7px;
+    }
   }
-}
-.ingredients {
-  flex: 1;
-}
-table {
-  table-layout: fixed;
-  width: 100%;
-}
-
-tr {
-  line-height: 25px;
-}
-.userInput {
-  display: flex;
-  flex-direction: column;
-  align-self: flex-end;
-}
-input {
-  @include button(248px, 50px, #fffdd3, 17px);
-  text-align: center;
-  cursor: text;
-  color: $darkNavy;
-  margin-right: -1px;
-  margin-bottom: -1px;
-}
-input:focus,
-text-area:focus {
-  outline-width: 0px;
-}
-button {
-  @include button(250px, 50px, $yellow, 17px);
-  margin-right: -1px;
-  margin-bottom: 0px;
+  @include mobile {
+    margin-left: 40px;
+    margin-top: 10px;
+    width: 260px;
+  }
 }
 h3 {
   @include h3;
@@ -82,6 +70,63 @@ h3 {
   margin-bottom: 20px;
   border-bottom: 1px solid $lilac;
   width: 133px;
+}
+.ingredients {
+  flex: 1;
+  @include mobile {
+    margin-left: 40px;
+    margin-bottom: 30px;
+  }
+}
+table {
+  table-layout: fixed;
+  width: 100%;
+}
+tr {
+  line-height: 25px;
+  @include mobile {
+    line-height: 23px;
+  }
+}
+.userInput {
+  display: flex;
+  flex-direction: column;
+  align-self: flex-end;
+  @include mobile {
+    display: inline;
+    margin-left: 30px;
+  }
+}
+input {
+  @include button(248px, 50px, #fffdd3, 17px);
+  text-align: center;
+  cursor: text;
+  color: $darkNavy;
+  margin-right: -1px;
+  border-radius: 1px 7px 0px 0px;
+  @include mobile {
+    height: 30px;
+    font-size: 15px;
+    width: 258px;
+  }
+}
+input:focus,
+text-area:focus {
+  outline-style: none;
+}
+button {
+  @include defaultButton;
+  margin-right: -1px;
+  margin-bottom: 0px;
+  outline-style: none;
+  border-radius: 0px 0px 1px 7px;
+  @include mobile {
+    margin-left: 30px;
+    height: 30px;
+    font-size: 15px;
+    width: 260px;
+    margin-bottom: 30px;
+  }
 }
 </style>
 
@@ -111,6 +156,56 @@ export default {
       });
       console.log("shoppingList: ", shoppingList);
       this.$emit("shoppingListWasUpdated", shoppingList);
+    },
+    pluraliseUnit(amount, unit) {
+      if (unit == "dash") {
+        if (amount == 1) {
+          return "dash";
+        } else return "dashes";
+      } else if (unit == "twist") {
+        if (amount == 1) {
+          return "twist";
+        } else return "twists";
+      } else if (unit == "lime") {
+        if (amount == 1) {
+          return "lime";
+        } else return "limes";
+      } else if (unit == "tsp") {
+        if (amount == 1) {
+          return "tsp";
+        } else return "tsps";
+      } else if (unit == "cube") {
+        if (amount == 1) {
+          return "cube";
+        } else return "cubes";
+      } else if (unit == "cherry") {
+        if (amount == 1) {
+          return "cherry";
+        } else return "cherries";
+      } else if (unit == "lemon") {
+        if (amount == 1) {
+          return "lemon";
+        } else return "lemons";
+      } else if (unit == "slice") {
+        if (amount == 1) {
+          return "slice";
+        } else return "slices";
+      } else if (unit == "leaf") {
+        if (amount == 1) {
+          return "leaf";
+        } else return "leaves";
+      } else if (unit == "oz") {
+        return "ml";
+      } else if (unit == "drop") {
+        if (amount == 1) {
+          return "drop";
+        } else return "drops";
+      } else return unit;
+    },
+    convertOzToMl(amount, unit) {
+      if (unit == "oz") {
+        return amount * 30;
+      } else return amount;
     }
   }
 };
